61dce3fa5ca947003020255b1064a7c71a7759e9
Kameleo Team <hello@kameleo.io>
2020-12-21 14:55:41 +0100

    ~ update xml comments

--- src/builderForCreateProfile.ts
+++ src/builderForCreateProfile.ts
@@ -30,12 +30,24 @@ export class BuilderForCreateProfile {
         return result;
     }
 
+    /**
+     * @summary <para>Tells the mode how the canvas will be spoofed. Possible values:</para>
+     * <para>'noise': Add some noise to the Canvas generation.</para>
+     * <para>'block': Completely block the 2D API.</para>
+     * <para>'off': Turn off the spoofing, use the original settings.</para>
+     */
     public setCanvas(value: Canvas): BuilderForCreateProfile {
         this.profileRequest.canvas = value;
 
         return this;
     }
 
+    /**
+     * @summary <para>Set the Webgl spoofing. Possible values:</para>
+     * <para>'noise': Add some noise to the WebGL generation</para>
+     * <para>'block': Completely block the 3D API</para>
+     * <para>'off': Turn off the spoofing, use the original settings</para>
+     */
     public setWebgl(value: Value, options: WebglSpoofingOptions|undefined): BuilderForCreateProfile {
         this.profileRequest.webgl.value = value;
         this.profileRequest.webgl.extra = options;
@@ -43,6 +55,13 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the Timezone will be spoofed. Possble values:</para>
+     * <para>'automatic': Timezone is automatically set by the IP</para>
+     * <para>'manual': Timezone is manually overridden in the profile</para>
+     * <para>'off': Turn off the spoofing, use the original settings</para>
+     * @param options When the Timezone spoofing is set to manual the timezone in Iana format is required. For example: America/Grenada
+     */
     public setTimezone(value: Value1, options: string|undefined): BuilderForCreateProfile {
         this.profileRequest.timezone.value = value;
         this.profileRequest.timezone.extra = options;
@@ -50,6 +69,13 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the Geolocation will be spoofed. Possible values:</para>
+     * <para>'automatic': Automatically set the values based on the IP address</para>
+     * <para>'manual': Manually set the longitude and latitude in the profile</para>
+     * <para>'block': Completely block the GeolocationAPI</para>
+     * <para>'off': Turn off the spoofing, use the original settings</para>
+     */
     public setGeolocation(value: Value2, options: GeolocationSpoofingOptions|undefined): BuilderForCreateProfile {
         this.profileRequest.geolocation.value = value;
         this.profileRequest.geolocation.extra = options;
@@ -57,6 +83,13 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Proxy connection settings of the profiles. Possible values:</para>
+     * <para>'none': Direct connection without any proxy.</para>
+     * <para>'http': Use a HTTP(S) proxy for upstream communication.</para>
+     * <para>'socks5': Use a SOCKS5 proxy for upstream communication.</para>
+     * <para>'ssh': Use an SSH connection for upstream communication. Basically a SOCKS5 proxy created at the given SSH host.</para>
+     */
     public setProxy(value: Value3, options: Server|undefined): BuilderForCreateProfile {
         this.profileRequest.proxy.value = value;
         this.profileRequest.proxy.extra = options;
@@ -64,6 +97,13 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the WebRTC will be spoofed. Possible values:</para>
+     * <para>'automatic': Automatically set the webRTC public IP by the IP, and generates a random private IP like '2d2f78e7-1b1e-4345-a21b-07c904c98394.local'</para>
+     * <para>'manual': Manually override the webRTC public IP and private IP in the profile</para>
+     * <para>'block': Block the WebRTC functionality</para>
+     * <para>'off': Turn off the spoofing, use the original settings</para>
+     */
     public setWebRtc(value: Value2, options: WebRtcSpoofingOptions|undefined): BuilderForCreateProfile {
         this.profileRequest.webRtc.value = value;
         this.profileRequest.webRtc.extra = options;
@@ -71,6 +111,11 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the Fonts will be spoofed. Possible values:</para>
+     * <para>'enabled': Enable fonts spoofing. A list can be provided to override the fonts coming from the base profile.</para>
+     * <para>'disable': Disable fonts spoofing.</para>
+     */
     public setFonts(value: Value6, options: string[]|undefined): BuilderForCreateProfile {
         this.profileRequest.fonts.value = value;
         this.profileRequest.fonts.extra = options;
@@ -78,6 +123,11 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the Plugins will be spoofed.</para>
+     * <para>'enabled': Enable plugins spoofing. A list can be provided to EXCLUDE plugins from the plugins of the base profile.</para>
+     * <para>'disabled': Disable plugins spoofing.</para>
+     */
     public setPlugins(value: Value6, options: string[]|undefined): BuilderForCreateProfile {
         this.profileRequest.plugins.value = value;
         this.profileRequest.plugins.extra = options;
@@ -85,12 +135,21 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary This website will be opened in the browser when the profile launches.
+     */
     public setStartPage(value: string): BuilderForCreateProfile {
         this.profileRequest.startPage = value;
 
         return this;
     }
 
+    /**
+     * @summary <para>Tells the mode how the screen will be spoofed. Possible values:</para>
+     * <para>'automatic': Automatically override the screen resolution based on the Base Profile.</para>
+     * <para>'manual': Manually override the screen resolution.</para>
+     * <para>'off': Turn off the spoofing, use the original settings.</para>
+     */
     public setScreen(value: Value1, options: string|undefined): BuilderForCreateProfile {
         this.profileRequest.screen.value = value;
         this.profileRequest.screen.extra = options;
@@ -98,24 +157,42 @@ export class BuilderForCreateProfile {
         return this;
     }
 
+    /**
+     * @summary A list of abolute paths from where the profile should load extensions or addons when starting the browser. For chrome and edge use CRX3 format extensions. For firefox use signed xpi format addons.
+     */
     public setExtensions(absolutePaths: string[]): BuilderForCreateProfile {
         this.profileRequest.extensions = absolutePaths;
 
         return this;
     }
 
+    /**
+     * @summary A free text including any notes written by the user.
+     */
     public setNotes(notes: string): BuilderForCreateProfile {
         this.profileRequest.notes = notes;
 
         return this;
     }
 
+    /**
+     * @summary <para>The mode how the profile should be launched. It determines which browser to launch. This cannot be modified after creation. Possible values are:</para>
+     * <para>'automatic': Automatically choose launcher based on DeviceType and BrowserProduct property.</para>
+     * <para>'chrome': Forcefully start the profile in Chrome.</para>
+     * <para>'chromium': Forcefully start the profile in Chromium.</para>
+     * <para>'firefox': Forcefully start the profile in Firefox.</para>
+     * <para>'edge': Forcefully start the profile in Edge.</para>
+     * <para>'external': Only start the external spoofing engine and connect any browser manually. This is also used for Mobile Device spoofing.</para>
+     */
     public setLauncher(browserLauncher: string): BuilderForCreateProfile {
         this.profileRequest.launcher = browserLauncher;
 
         return this;
     }
 
+    /**
+     * @summary This sets all the profile options to the defaults recommended by Kameleo Team. Please consider providing Proxy settings to your profile.
+     */
     public setRecommendedDefaults(): BuilderForCreateProfile {
         this.profileRequest.canvas = "noise";
         this.profileRequest.webgl.value = "noise";
